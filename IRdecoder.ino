41d06760429cc152798d627ee0ce3831a9d6a185
amanadhav <amanadhav@gmail.com>
2016-09-16 19:35:47 -0400

    Add files via upload

---
+++ IRdecoder.ino
@@ -0,0 +1,87 @@
+
+
+
+#define IRpin_PIN PIND
+#define IRpin 2
+
+
+// the maximum pulse we'll listen for - 65 milliseconds is a long time
+#define MAXPULSE 65000
+
+// what our timing resolution should be
+// accurate timing
+#define RESOLUTION 20
+
+// store up to 100 pulse pairs
+uint16_t pulses[100][2]; // pair is high and low pulse
+uint8_t currentpulse = 0; // index for pulses we're storing
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println("DECODING!");
+}
+
+void loop(void) {
+  uint16_t highpulse, lowpulse; // temporary storage timing
+  highpulse = lowpulse = 0; // start out with no pulse length
+  
+  
+// while (digitalRead(IRpin)) { 
+    while (IRpin_PIN & (1 << IRpin)) {
+     // pin is still HIGH
+
+     // count off another few microseconds
+     highpulse++;
+     delayMicroseconds(RESOLUTION);
+
+     // If the pulse is too long, wait
+     if ((highpulse >= MAXPULSE) && (currentpulse != 0)) {
+       printpulses();
+       currentpulse=0;
+       return;
+     }
+  }
+  // we didn't time out so lets stash the reading
+  pulses[currentpulse][0] = highpulse;
+  
+  // same as above
+  while (! (IRpin_PIN & _BV(IRpin))) {
+     // pin is still LOW
+     lowpulse++;
+     delayMicroseconds(RESOLUTION);
+     if ((lowpulse >= MAXPULSE) && (currentpulse != 0)) {
+       printpulses();
+       currentpulse=0;
+       return;
+     }
+  }
+  pulses[currentpulse][1] = lowpulse;
+
+  // High-low pulse succeddful
+  currentpulse++;
+}
+
+void printpulses(void) {
+  Serial.println("\n\r\n\rReceived: \n\rOFF \tON");
+  for (uint8_t i = 0; i < currentpulse; i++) {
+    Serial.print(pulses[i][0] * RESOLUTION, DEC);
+    Serial.print(" usec, ");
+    Serial.print(pulses[i][1] * RESOLUTION, DEC);
+    Serial.println(" usec");
+  }
+ 
+  // print it in a 'array' format
+  Serial.println("int IRsignal[] = {");
+  Serial.println("// ON, OFF (in 10's of microseconds)");
+  for (uint8_t i = 0; i < currentpulse-1; i++) {
+    Serial.print("\t"); // tab
+    Serial.print(pulses[i][1] * RESOLUTION / 10, DEC);
+    Serial.print(", ");
+    Serial.print(pulses[i+1][0] * RESOLUTION / 10, DEC);
+    Serial.println(",");
+  }
+  Serial.print("\t"); // tab
+  Serial.print(pulses[currentpulse-1][1] * RESOLUTION / 10, DEC);
+  Serial.print(", 0};");
+}
+
---
+++ PlaySignal.ino
@@ -0,0 +1,97 @@
+  
+  #define IRledPin 12
+  #define NumIRsignals 96
+        
+  // This is the code I determined works for my Duraflame heater
+  const int buttonPin = 4;
+  int buttonState = 0; 
+  int IRsignal[] = {
+  // ON, OFF (in 10's of microseconds)
+    876, 430,
+    56, 164,
+    54, 162,
+    56, 52,
+    56, 54,
+    54, 164,
+    54, 164,
+    54, 54,
+    54, 54,
+    54, 54,
+    54, 54,
+    54, 54,
+    54, 54,
+    54, 54,
+    56, 52,
+    56, 52,
+    56, 52,
+    56, 52,
+    54, 54,
+    54, 54,
+    54, 54,
+    56, 52,
+    56, 52,
+    56, 52,
+    56, 52,
+    56, 162,
+    56, 162,
+    56, 164,
+    54, 162,
+    56, 164,
+    54, 164,
+    54, 164,
+    54, 164,
+    54, 4310,
+    876, 214,
+    54, 0};
+  
+  void setup(void) {
+    digitalWrite(IRledPin, LOW);   //LED starts "off"
+    Serial.begin(9600);            //Initialize Serial port
+    pinMode(buttonPin, INPUT);
+  }
+  
+  void loop() {
+    char data[6];
+    int index = 0;
+    buttonState = digitalRead(buttonPin);
+    delay(1000);  //Serial input seems to need some kind of short delay or the data gets screwed up.
+  
+    while (Serial.available() > 0) {            //Loop if there data on the serial line
+      if (index < 5) {                          //Make sure we don't overflow
+        data[index] = Serial.read();            //Load a character into the string
+        index++;                                //Increment the index to get the next character
+      }
+    }
+  
+    data[5]='\0';  //Null terminate the string
+    
+    if (strcmp(data, "POWER") == 0){                //If the Arduino receives the POWER signal...
+      Serial.println("SENDING SIGNAL!");
+      for (int i = 0; i < NumIRsignals; i+=2) {         //Loop through all of the IR timings
+        pulseIR(IRsignal[i]*10);              //Flash IR LED at 38khz for the right amount of time
+        delayMicroseconds(IRsignal[i+1]*10);  //Then turn it off for the right amount of time
+      }
+    }                                         //Otherwise do nothing!
+  }
+  
+  // This function allows us to PWM the IR LED at about 38khz for the sensor
+  
+  // THIS IS THE LADYADA program! Has been copied
+  void pulseIR(long microsecs) {
+    // we'll count down from the number of microseconds we are told to wait
+   
+    cli();  // this turns off any background interrupts
+   
+    while (microsecs > 0) {
+      // 38 kHz is about 13 microseconds high and 13 microseconds low
+     digitalWrite(IRledPin, HIGH);  // this takes about 3 microseconds to happen
+     delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+     digitalWrite(IRledPin, LOW);   // this also takes about 3 microseconds
+     delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+   
+     // so 26 microseconds altogether
+     microsecs -= 26;
+    }
+   
+    sei();  // this turns them back on
+  }
